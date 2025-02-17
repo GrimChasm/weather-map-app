4335544a3f25b4fb1fff43b4c47313873991f195
GrimChasm <121305118+GrimChasm@users.noreply.github.com>
2025-02-16 18:44:02 -0600

    Update liveUSweatherapp.py

--- liveUSweatherapp.py
+++ liveUSweatherapp.py
@@ -27,8 +27,8 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
 
